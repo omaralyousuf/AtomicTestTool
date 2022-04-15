a9f1539a69903733cb752aa5b6644367bd59e560
Yousuf <oyousuf@NTI.local>
2022-04-15 11:33:14 -0500

    Updates

--- AtomicTestTool/Program.cs
+++ AtomicTestTool/Program.cs
@@ -14,54 +14,82 @@ namespace AtomicTestTool
         public static void Main()
         {
             try
-            {   
+            {
                 using (Process process = new Process())
                 {
-                    
-                // Atomic attack commands fed in from builder
-                string[] cmdCommands = {
-                "replace-here"
-                };
-                    //Start command prompt process
-                    process.StartInfo.CreateNoWindow = true;
-                    process.StartInfo.UseShellExecute = false;
-                    process.StartInfo.RedirectStandardOutput = true;
-                    process.StartInfo.RedirectStandardError = true;
-                    process.StartInfo.RedirectStandardInput = true;
-                    process.StartInfo.FileName = "cmd.exe";
 
-                    //Loop thru the array of commands
-                    string strCommand = "";
-                    for (int command = 0; command < cmdCommands.Length; command++)
+                    // Atomic attack commands fed in from builder
+                    string[] cmdCommands = {
+                    "replace-here"
+                    };
+
+                    string[] psCommands = {
+                        "-c"
+                    };
+
+                    //Method 1 that runs attacks using cmd.exe
+                    if (cmdCommands.Length > 12)
                     {
-                        if (cmdCommands[command] != "")
+                        //Start command prompt process
+                        process.StartInfo.CreateNoWindow = true;
+                        process.StartInfo.UseShellExecute = false;
+                        process.StartInfo.RedirectStandardOutput = true;
+                        process.StartInfo.RedirectStandardError = true;
+                        process.StartInfo.RedirectStandardInput = true;
+                        process.StartInfo.FileName = "cmd.exe";
+
+                        //Loop thru the array of commands
+                        string strCommand = "";
+                        for (int command = 0; command < cmdCommands.Length; command++)
                         {
-                            strCommand = strCommand + cmdCommands[command] + " & ";
+                            if (cmdCommands[command] != "")
+                            {
+                                strCommand = strCommand + cmdCommands[command] + " & ";
+                            }
                         }
-                    }
-
-                    // Command Line
-                    if (strCommand.Length < 40)
-                    {
                         process.StartInfo.Arguments = "/C " + strCommand;
+                        process.Start();
+                        process.StandardInput.Close();
+                        var lines = process.StandardOutput.ReadToEnd();
+                        process.WaitForExit();
+
+                        //Uncomment the two line below to show the cmd window
+                        Console.WriteLine(lines);
+                        Console.ReadKey();
                     }
 
-                    // Powershell
-                    else
+                    else if (psCommands.Length > 2)
                     {
-                    process.StartInfo.Arguments = "/C Powershell -Command " + strCommand;
+                        //Method 2 runs attacks using Powershell
+                        //Start command prompt process
+                        process.StartInfo.CreateNoWindow = true;
+                        process.StartInfo.UseShellExecute = false;
+                        process.StartInfo.RedirectStandardOutput = true;
+                        process.StartInfo.RedirectStandardError = true;
+                        process.StartInfo.RedirectStandardInput = true;
+                        process.StartInfo.FileName = "cmd.exe";
+
+                        //Loop thru the array of commands
+                        string strCommand = "";
+                        for (int command = 0; command < psCommands.Length; command++)
+                        {
+                            if (psCommands[command] != "")
+                            {
+                                strCommand = strCommand + psCommands[command] + " & ";
+                            }
+                        }
+                        process.StartInfo.Arguments = "/C Powershell -Command " + strCommand;
+                        process.Start();
+                        process.StandardInput.Close();
+                        var lines = process.StandardOutput.ReadToEnd();
+                        process.WaitForExit();
+
+                        //Uncomment the two line below to show the cmd window
+                        Console.WriteLine(lines);
+                        Console.ReadKey();
                     }
-
-                    process.Start();
-                    process.StandardInput.Close();
-                    var lines = process.StandardOutput.ReadToEnd();
-                    process.WaitForExit();
-
-                    //Uncomment the two line below to show the cmd window
-                    Console.WriteLine(lines);
-                    Console.ReadKey();
                 }
-              
+
             }
             catch (Exception e)
             {
