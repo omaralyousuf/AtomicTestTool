cae36fbb49e0b08daf56eb1b500f3a9f65dd2ac5
Yousuf <oyousuf@NTI.local>
2022-04-15 01:41:48 -0500

    Updates

--- AtomicTestTool/Program.cs
+++ AtomicTestTool/Program.cs
@@ -18,54 +18,48 @@ namespace AtomicTestTool
                 using (Process process = new Process())
                 {
                     
-                    // Atomic attack commands fed in from builder
-                    string[] cmdCommands = {
-                    "replace-here"
-                    };
+                // Atomic attack commands fed in from builder
+                string[] cmdCommands = {
+                "replace-here"
+                };
+                    //Start command prompt process
+                    process.StartInfo.CreateNoWindow = true;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.RedirectStandardError = true;
+                    process.StartInfo.RedirectStandardInput = true;
+                    process.StartInfo.FileName = "cmd.exe";
 
-                    //Method 1 that runs attacks using cmd.exe
-                    if (cmdCommands.Length > 0)
+                    //Loop thru the array of commands
+                    string strCommand = "";
+                    for (int command = 0; command < cmdCommands.Length; command++)
                     {
-                        //Start command prompt process
-                        process.StartInfo.CreateNoWindow = true;
-                        process.StartInfo.UseShellExecute = false;
-                        process.StartInfo.RedirectStandardOutput = true;
-                        process.StartInfo.RedirectStandardError = true;
-                        process.StartInfo.RedirectStandardInput = true;
-                        process.StartInfo.FileName = "cmd.exe";
-
-                        //Loop thru the array of commands
-                        string strCommand = "";
-                        for (int command = 0; command < cmdCommands.Length; command++)
+                        if (cmdCommands[command] != "")
                         {
-                            if (cmdCommands[command] != "")
-                            {
-                                strCommand = strCommand + cmdCommands[command] + " & ";
-                            }
+                            strCommand = strCommand + cmdCommands[command] + " & ";
                         }
-
-                        // Command Line
-                        if (strCommand.Length < 40)
-                        {
-                            process.StartInfo.Arguments = "/C " + strCommand;
-                        }
-
-                        // Powershell
-                        else
-                        {
-                        process.StartInfo.Arguments = "/C Powershell -Command " + strCommand;
-                        }
-
-                        process.Start();
-                        process.StandardInput.Close();
-                        var lines = process.StandardOutput.ReadToEnd();
-                        process.WaitForExit();
-
-                        //Uncomment the two line below to show the cmd window
-                        Console.WriteLine(lines);
-                        Console.ReadKey();
                     }
 
+                    // Command Line
+                    if (strCommand.Length < 40)
+                    {
+                        process.StartInfo.Arguments = "/C " + strCommand;
+                    }
+
+                    // Powershell
+                    else
+                    {
+                    process.StartInfo.Arguments = "/C Powershell -Command " + strCommand;
+                    }
+
+                    process.Start();
+                    process.StandardInput.Close();
+                    var lines = process.StandardOutput.ReadToEnd();
+                    process.WaitForExit();
+
+                    //Uncomment the two line below to show the cmd window
+                    Console.WriteLine(lines);
+                    Console.ReadKey();
                 }
               
             }
